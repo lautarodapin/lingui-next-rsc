29dbb61f07dc7b200cb43904393386cf46a42824
lautarodapin <lautarodapin@gmail.com>
2024-04-30 20:06:01 -0300

    test link

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -1,10 +1,15 @@
 'use client'
 import {Trans} from '@lingui/macro';
+import Link from 'next/link';
 
 export default function Home() {
     return (
         <main>
-            <Trans>Hello</Trans>
+            <div style={{display: 'flex', flexDirection: 'column', gap: '8px'}}>
+                <Trans>Hello</Trans>
+                <Link href={'/es-es'}>Es</Link>
+                <Link href={'/en-us'}>en</Link>
+            </div>
         </main>
     );
 }
